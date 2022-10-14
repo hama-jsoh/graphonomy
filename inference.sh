7ae6543f71e18e57f4977a2e6282806eb045b1be
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-14 16:07:45 +0900

    :hammer: Update script

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 python exp/inference/inference.py \
 	--loadmodel ./data/pretrained_model/inference.pth \
-	--img_path ./img/messi.jpg \
+	--img_path ./img/$1.jpg \
 	--output_path ./img/ \
-	--output_name output_name
+	--output_name $1
